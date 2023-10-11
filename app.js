42d63a1a5113514d48c72f2a4cbeee3878a86534
Gana <ganafall9498@gmail.com>
2023-10-11 15:43:20 +0200

    Ajout .gitignore et app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const nodemon = require('nodemon')
+const app = express()
+const port = 3000
+
+
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://127.0.0.1:27017/apinode');
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+// ancienne version musiqueRoute(app);
+ const musiqueRoute = require('./routes/musiqueRoute');
+ const voteRoute = require('./routes/voteRoute');
+// nouvelle version
+app.use('/musiques', musiqueRoute);
+app.use('/', voteRoute);
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
